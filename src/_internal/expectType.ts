8ee606a8d32ef25bc0f416dcef1f398e9a3d9fc0
poteat <poteat@users.noreply.github.com>
2024-10-03 20:01:46 -0700

    feat: initialize project structure

---
+++ src/_internal/expectType.ts
@@ -0,0 +1,9 @@
+import { Type } from "hkt-toolbelt";
+
+export function expectType<T>(value: T) {
+  return {
+    toBe: <U extends T>(expected: Type._$infer<U>) => {
+      expect(value).toBe(expected);
+    },
+  };
+}
